876bea61bbbf10c9396a15b0dbf1bbde6a37c8bb
Faisal <king975232@gmail.com>
2021-04-29 04:43:08 +0530

    my project

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within 
+r API!
+|
+*/
+Route::post('login', 'API\UserController@login');
+Route::post('register', 'API\UserController@register');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
